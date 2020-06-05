d40b3b25d03dc6213e23c85b8619843021a646e8
crazyq <crazyq@gmail.com>
2020-06-05 18:04:18 +0800

    汉化设置菜单

--- libretro_core_options.h
+++ libretro_core_options.h
@@ -1,4 +1,4 @@
-#ifndef LIBRETRO_CORE_OPTIONS_H__
+﻿#ifndef LIBRETRO_CORE_OPTIONS_H__
 #define LIBRETRO_CORE_OPTIONS_H__
 
 #include <stdlib.h>
@@ -638,6 +638,597 @@ struct retro_core_option_definition option_defs_us[] = {
 /* RETRO_LANGUAGE_CHINESE_TRADITIONAL */
 
 /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
+struct retro_core_option_definition option_defs_chs[] = {
+   {
+      "pce_cdimagecache",
+      "CD镜像缓存（须重启）",
+      "读取CD镜像到内存。",
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_cdbios",
+      "CD BIOS（须重启）",
+      "大部分游戏可以在'System Card 3'下运行，少数未授权游戏需要'Games Express'。",
+      {
+         { "Games Express", NULL },
+         { "System Card 1", NULL },
+         { "System Card 2", NULL },
+         { "System Card 3", NULL },
+         { "System Card 2 US", NULL },
+         { "System Card 3 US", NULL },
+         { NULL, NULL},
+      },
+      "System Card 3"
+   },
+   {
+      "pce_arcadecard",
+      "街机卡（须重启）",
+      "保持此项启用，以允许ACD-enhanced SCD游戏的增强模式。",
+      {
+         { "enabled", NULL },
+         { "disabled", NULL },
+         { NULL, NULL},
+      },
+      "enabled"
+   },
+   {
+      "pce_nospritelimit",
+      "无角色限制",
+      "移除每扫描线16个角色的硬件限制。\n"
+      "警告：某些游戏可能会产生图像错误（例如Bloody Wolf）。",
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_ocmultiplier",
+      "CPU超频倍率",
+      "较高的值可以减少游戏拖慢现象。\n"
+      "警告：可能导致错误或崩溃。",
+      {
+         { "1", NULL },
+         { "2", NULL },
+         { "3", NULL },
+         { "4", NULL },
+         { "5", NULL },
+         { "6", NULL },
+         { "7", NULL },
+         { "8", NULL },
+         { "9", NULL },
+         { "10", NULL },
+         { "20", NULL },
+         { "30", NULL },
+         { "40", NULL },
+         { "50", NULL },
+         { NULL, NULL},
+      },
+      "1"
+   },
+   {
+      "pce_aspect_ratio",
+      "输出宽高比",
+      "选择输出宽高比。\n"
+      "此项仅在RetroArch的宽高比设置为'内核提供'时有效。",
+      {
+         { "auto", "自动" },
+         { "6:5", NULL },
+         { "4:3", NULL },
+         { "uncorrected", "无纠正的" },
+         { NULL, NULL},
+      },
+      "auto"
+   },
+   {
+      "pce_scaling",
+      "Resolution Scaling",
+      "'auto' will authorize resolution to switch. 'lores' can crush pixels. 'hires' will stay on the maximum width.",
+      {
+         { "auto", NULL },
+         { "lores", NULL },
+         { "hires", NULL },
+         { NULL, NULL},
+      },
+      "auto"
+   },
+   {
+      "pce_hires_blend",
+      "Hires Blending Strength",
+      "Blend pixels together when in hires mode. Higher values will blur the picture more.",
+      {
+         { "disabled", NULL },
+         { "1", NULL },
+         { "2", NULL },
+         { "3", NULL },
+         { "4", NULL },
+         { "5", NULL },
+         { "6", NULL },
+         { "7", NULL },
+         { "8", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_h_overscan",
+      "Show Horizontal Overscan",
+      "Show or crop horizontal overscan. 'auto' will try to adapt to games, cropping empty areas.",
+      {
+         { "auto", NULL },
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "auto"
+   },
+   {
+      "pce_initial_scanline",
+      "Initial Scanline",
+      "First rendered scanline. Higher values will crop the top of the screen.",
+      {
+         { "0", NULL },
+         { "1", NULL },
+         { "2", NULL },
+         { "3", NULL },
+         { "4", NULL },
+         { "5", NULL },
+         { "6", NULL },
+         { "7", NULL },
+         { "8", NULL },
+         { "9", NULL },
+         { "10", NULL },
+         { "11", NULL },
+         { "12", NULL },
+         { "13", NULL },
+         { "14", NULL },
+         { "15", NULL },
+         { "16", NULL },
+         { "17", NULL },
+         { "18", NULL },
+         { "19", NULL },
+         { "20", NULL },
+         { "21", NULL },
+         { "22", NULL },
+         { "23", NULL },
+         { "24", NULL },
+         { "25", NULL },
+         { "26", NULL },
+         { "27", NULL },
+         { "28", NULL },
+         { "29", NULL },
+         { "30", NULL },
+         { "31", NULL },
+         { "32", NULL },
+         { "33", NULL },
+         { "34", NULL },
+         { "35", NULL },
+         { "36", NULL },
+         { "37", NULL },
+         { "38", NULL },
+         { "39", NULL },
+         { "40", NULL },
+         { NULL, NULL},
+      },
+      "3"
+   },
+   {
+      "pce_last_scanline",
+      "Last Scanline",
+      "Last rendered scanline. Lower values will crop the bottom of the screen.",
+      {
+         { "208", NULL },
+         { "209", NULL },
+         { "210", NULL },
+         { "211", NULL },
+         { "212", NULL },
+         { "213", NULL },
+         { "214", NULL },
+         { "215", NULL },
+         { "216", NULL },
+         { "217", NULL },
+         { "218", NULL },
+         { "219", NULL },
+         { "220", NULL },
+         { "221", NULL },
+         { "222", NULL },
+         { "223", NULL },
+         { "224", NULL },
+         { "225", NULL },
+         { "226", NULL },
+         { "227", NULL },
+         { "228", NULL },
+         { "229", NULL },
+         { "230", NULL },
+         { "231", NULL },
+         { "232", NULL },
+         { "233", NULL },
+         { "234", NULL },
+         { "235", NULL },
+         { "236", NULL },
+         { "237", NULL },
+         { "238", NULL },
+         { "239", NULL },
+         { "240", NULL },
+         { "241", NULL },
+         { "242", NULL },
+         { NULL, NULL},
+      },
+      "242"
+   },
+   {
+      "pce_psgrevision",
+      "PSG Audio Chip (Restart)",
+      "HuC6280 as found in the original PC Engine. HuC6280A as found in the SuperGrafx and CoreGrafx I.",
+      {
+         { "auto", NULL },
+         { "HuC6280", NULL },
+         { "HuC6280A", NULL },
+         { NULL, NULL},
+      },
+      "HuC6280A"
+   },
+   {
+      "pce_cddavolume",
+      "(CD) CDDA Volume %",
+      "CD game only. Setting this volume control too high may cause sample clipping.",
+      {
+         { "0", NULL },
+         { "10", NULL },
+         { "20", NULL },
+         { "30", NULL },
+         { "40", NULL },
+         { "50", NULL },
+         { "60", NULL },
+         { "70", NULL },
+         { "80", NULL },
+         { "90", NULL },
+         { "100", NULL },
+         { "110", NULL },
+         { "120", NULL },
+         { "130", NULL },
+         { "140", NULL },
+         { "150", NULL },
+         { "160", NULL },
+         { "170", NULL },
+         { "180", NULL },
+         { "190", NULL },
+         { "200", NULL },
+         { NULL, NULL},
+      },
+      "100"
+   },
+   {
+      "pce_adpcmvolume",
+      "(CD) ADPCM Volume %",
+      "CD game only. Setting this volume control too high may cause sample clipping.",
+      {
+         { "0", NULL },
+         { "10", NULL },
+         { "20", NULL },
+         { "30", NULL },
+         { "40", NULL },
+         { "50", NULL },
+         { "60", NULL },
+         { "70", NULL },
+         { "80", NULL },
+         { "90", NULL },
+         { "100", NULL },
+         { "110", NULL },
+         { "120", NULL },
+         { "130", NULL },
+         { "140", NULL },
+         { "150", NULL },
+         { "160", NULL },
+         { "170", NULL },
+         { "180", NULL },
+         { "190", NULL },
+         { "200", NULL },
+         { NULL, NULL},
+      },
+      "100"
+   },
+   {
+      "pce_cdpsgvolume",
+      "(CD) CD PSG Volume %",
+      "CD game only. Setting this volume control too high may cause sample clipping.",
+      {
+         { "0", NULL },
+         { "10", NULL },
+         { "20", NULL },
+         { "30", NULL },
+         { "40", NULL },
+         { "50", NULL },
+         { "60", NULL },
+         { "70", NULL },
+         { "80", NULL },
+         { "90", NULL },
+         { "100", NULL },
+         { "110", NULL },
+         { "120", NULL },
+         { "130", NULL },
+         { "140", NULL },
+         { "150", NULL },
+         { "160", NULL },
+         { "170", NULL },
+         { "180", NULL },
+         { "190", NULL },
+         { "200", NULL },
+         { NULL, NULL},
+      },
+      "100"
+   },
+   {
+      "pce_cdspeed",
+      "(CD) CD Speed",
+      "Faster loading times but can cause issues with a couple of games.",
+      {
+         { "1", NULL },
+         { "2", NULL },
+         { "4", NULL },
+         { "8", NULL },
+         { NULL, NULL},
+      },
+      "1"
+   },
+   {
+      "pce_adpcmextraprec",
+      "(CD) ADPCM precision",
+      "Full precision of 12-bits for the MSM5205 ADPCM predictor can reduce whining noise during ADPCM playback.",
+      {
+         { "10-bit", NULL },
+         { "12-bit", NULL },
+         { NULL, NULL},
+      },
+      "10-bit"
+   },
+   {
+      "pce_resamp_quality",
+      "Owl Resampler Quality",
+      "Higher values give better signal noise ratio and preservation of higher frequencies, but increased computation, a bit of latency and clipping if the volume is set too high.",
+      {
+         { "0", NULL },
+         { "1", NULL },
+         { "2", NULL },
+         { "3", NULL },
+         { "4", NULL },
+         { "5", NULL },
+         { "6", NULL },
+         { NULL, NULL},
+      },
+      "3"
+   },
+   {
+      "pce_show_advanced_input_settings",
+      "Show Advanced Input/Turbo Settings",
+      "Show Multitap, Mouse, Turbo Buttons and advanced parameters. NOTE: You need to go back in game and re-enter the menu to refresh the list.",
+      {
+         { "enabled",  NULL },
+         { "disabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_mouse_sensitivity",
+      "Mouse Sensitivity",
+      "Higher values will make the mouse cursor move faster.",
+      {
+         { "0.125", NULL },
+         { "0.250", NULL },
+         { "0.375", NULL },
+         { "0.500", NULL },
+         { "0.625", NULL },
+         { "0.750", NULL },
+         { "0.875", NULL },
+         { "1.000", NULL },
+         { "1.125", NULL },
+         { "1.25", NULL },
+         { "1.50", NULL },
+         { "1.75", NULL },
+         { "2.00", NULL },
+         { "2.25", NULL },
+         { "2.50", NULL },
+         { "2.75", NULL },
+         { "3.00", NULL },
+         { "3.25", NULL },
+         { "3.50", NULL },
+         { "3.75", NULL },
+         { "4.00", NULL },
+         { "4.25", NULL },
+         { "4.50", NULL },
+         { "4.75", NULL },
+         { "5.00", NULL },
+         { NULL, NULL},
+      },
+      "1.25"
+   },
+   {
+      "pce_multitap",
+      "Multitap 5-port Controller",
+      "Enable multitap (TurboTap) emulation.",
+      {
+         { "enabled", NULL },
+         { "disabled", NULL },
+         { NULL, NULL},
+      },
+      "enabled"
+   },
+   {
+      "pce_disable_softreset",
+      "Disable Soft Reset (RUN+SELECT)",
+      "If set, when RUN+SEL are pressed simultaneously, disable both buttons temporarily.",
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_up_down_allowed",
+      "Allow UP+DOWN/LEFT+RIGHT",
+      "Enabling this will allow pressing / quickly alternating / holding both left and right (or up and down) directions at the same time. This may cause movement-based glitches.",
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_Turbo_Delay",
+      "Turbo Delay",
+      NULL,
+      {
+         { "Fast", NULL },
+         { "Medium", NULL },
+         { "Slow", NULL },
+         { NULL, NULL},
+      },
+      "Fast"
+   },
+   {
+      "pce_Turbo_Toggling",
+      "Turbo Hotkey",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "toggle", NULL },
+         { "always", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_turbo_toggle_hotkey",
+      "Alternate Turbo Hotkey",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p0_turbo_I_enable",
+      "P1 Turbo I",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p0_turbo_II_enable",
+      "P1 Turbo II",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p1_turbo_I_enable",
+      "P2 Turbo I",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p1_turbo_II_enable",
+      "P2 Turbo II",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p2_turbo_I_enable",
+      "P3 Turbo I",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p2_turbo_II_enable",
+      "P3 Turbo II",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p3_turbo_I_enable",
+      "P4 Turbo I",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p3_turbo_II_enable",
+      "P4 Turbo II",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p4_turbo_I_enable",
+      "P5 Turbo I",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   {
+      "pce_p4_turbo_II_enable",
+      "P5 Turbo II",
+      NULL,
+      {
+         { "disabled", NULL },
+         { "enabled", NULL },
+         { NULL, NULL},
+      },
+      "disabled"
+   },
+   { NULL, NULL, NULL, {{0}}, NULL },
+};
 
 /* RETRO_LANGUAGE_ESPERANTO */
 
@@ -670,7 +1261,7 @@ struct retro_core_option_definition *option_defs_intl[RETRO_LANGUAGE_LAST] = {
    NULL,           /* RETRO_LANGUAGE_RUSSIAN */
    NULL,           /* RETRO_LANGUAGE_KOREAN */
    NULL,           /* RETRO_LANGUAGE_CHINESE_TRADITIONAL */
-   NULL,           /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
+   option_defs_chs,/* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
    NULL,           /* RETRO_LANGUAGE_ESPERANTO */
    NULL,           /* RETRO_LANGUAGE_POLISH */
    NULL,           /* RETRO_LANGUAGE_VIETNAMESE */
